8ba4f97f8a0301a060379abd32bc14858ef738f1
joyqwang <joyqwang24@gmail.com>
2017-05-31 20:54:38 -0700

    W3D2
    Changes after W3D2

---
+++ Selfiegram/SelfieCell.swift
@@ -0,0 +1,30 @@
+//
+//  SelfieCell.swift
+//  Selfiegram
+//
+//  Created by Joy Qiaoyi Wang on 2017-05-31.
+//  Copyright © 2017 Joy Qiaoyi Wang. All rights reserved.
+//
+
+import UIKit
+
+class SelfieCell: UITableViewCell {
+
+    @IBOutlet weak var selfieImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
